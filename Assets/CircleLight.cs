eb3ebd26d469481a818d0c10c0b373eb8ba99f2a
nikkitwo <nikki860618@gmail.com>
2019-11-09 14:54:14 +0800

    Initial commit

---
+++ Assets/CircleLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class CircleLight : MonoBehaviour
+{
+    public Color tsToColor;
+    public float LoopTime = 0.7f;
+    Image _image;
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _image.DOColor(tsToColor, LoopTime).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+}
